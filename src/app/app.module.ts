7a175529ed06ec3780a3dc7faacc2b3328eff4b4
EstebanBELFIS <ebelfis@gmail.com>
2020-03-20 15:26:18 +0100

    Parametrage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module';
-=======
->>>>>>> bde425c7a0181dc1afb42aba4582a9b366d88ec7
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,12 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-<<<<<<< HEAD
-    BrowserModule,
-    AppRoutingModule
-=======
     BrowserModule
->>>>>>> bde425c7a0181dc1afb42aba4582a9b366d88ec7
   ],
   providers: [],
   bootstrap: [AppComponent]
