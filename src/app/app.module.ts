42011c1a51423e38d541f6c96f2c062305642d0c
antoinebouard <antoinebouardain@gmail.com>
2020-03-26 17:48:44 +0100

    Routes MedecinsListe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MedicamentsListeComponent } from './medicaments-liste/medicaments-liste.component';
 import { MedecinsListeComponent } from './medecins-liste/medecins-liste.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { VisitesListeComponent } from './visites-liste/visites-liste.component';
+
+const appRoutes: Routes = [
+  { path: '', component : AccueilComponent },
+  { path: 'medecins', component : MedecinsListeComponent},
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +24,8 @@ import { VisitesListeComponent } from './visites-liste/visites-liste.component';
     VisitesListeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
