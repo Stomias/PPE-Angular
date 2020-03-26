5d9d07ca473d6fc031850871ae874c9d3ab188aa
EstebanBELFIS <ebelfis@gmail.com>
2020-03-26 17:50:23 +0100

    NavBar/Routing/List-Medoc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MedicamentsListeComponent } from './medicaments-liste/medicaments-liste.component';
 import { MedecinsListeComponent } from './medecins-liste/medecins-liste.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { VisitesListeComponent } from './visites-liste/visites-liste.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'ListeMedicaments', component: MedicamentsListeComponent },
+  { path: 'ListeMedecins', component: MedecinsListeComponent },
+  { path: 'ListeVisites', component: VisitesListeComponent },
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { VisitesListeComponent } from './visites-liste/visites-liste.component';
     VisitesListeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
