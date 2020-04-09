7360aba59199a5e8ff6f8571c4e9e398f2789191
antoinebouard <antoinebouardain@gmail.com>
2020-04-09 15:57:40 +0200

    Rapport medecin + Modif infos médecin(pas fini)

--- src/app/medecins-liste/medecindetails/medecindetails.component.ts
+++ src/app/medecins-liste/medecindetails/medecindetails.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Medecin } from '../../models/Medecin.model';
+import { Rapport } from '../../models/Rapport.model';
 import { MedecinService } from '../../services/medecin.service';
+import { RapportService } from '../../services/rapport.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
@@ -11,11 +13,13 @@ import { Subscription } from 'rxjs';
 })
 export class MedecindetailsComponent implements OnInit {
 
-  medecins: any[];
+  medecins: Medecin[];
   medecinSubscription: Subscription;
+  rapports: Rapport[];
+  rapportSubscription: Subscription;
   url = '';
   id = [];
-  constructor(private router: Router, private medecinService: MedecinService) { }
+  constructor(private router: Router, private medecinService: MedecinService, private rapportService: RapportService) { }
 
   ngOnInit(): void {
     this.url = this.router.url.toString();
@@ -26,10 +30,24 @@ export class MedecindetailsComponent implements OnInit {
         this.medecins = medecins;
       }
     );
+    this.getRapportOfMedecin(this.id[2]);
+    this.rapportSubscription = this.rapportService.rapportSubject.subscribe(
+      (rapports: any[]) => {
+        this.rapports = rapports;
+      }
+    );
     this.medecinService.emitMedecinSubject();
   }
 
   getMedecinDetails(id: string) {
     this.medecinService.getMedecinByID(id);
+  }
+
+  getRapportOfMedecin(idMed: string) {
+    this.rapportService.getAllRapportMedecin(idMed);
+  }
+
+  updateMedecinDetails(id: string, adresse: string, tel: string, specomplementaire: string) {
+    this.medecinService.updateMedecinDetails(id, adresse, tel, specomplementaire);
   }
 }
--- src/app/services/rapport.service.ts
+++ src/app/services/rapport.service.ts
@@ -16,12 +16,23 @@ export class RapportService {
 
   constructor(private http: HttpClient) { }
 
-  // Recupère les rapports
+  // Recupère les rapports par rappport l'id du visiteur
   getAllRapport() {
     this.http.get<any[]>(`https://webserv-gr1.sio-carriat.com/gsbapi/?id5-2=c14`).subscribe(
       (response) => {
         this.rapports = response;
         console.log('Chargement réussie\n' + this.rapports[10]);
+        this.emitRapportSubject();
+      }
+    );
+  }
+
+  // Récupère les rapports par rappoort à l'id du médecin
+  getAllRapportMedecin(id: string) {
+    this.http.get<any[]>(`https://webserv-gr1.sio-carriat.com/gsbapi/?idRapport=${id}/`).subscribe(
+      (response) => {
+        this.rapports = response;
+        console.log('Chargement réussie\n' + this.rapports[0].id);
         this.emitRapportSubject();
       }
     );
